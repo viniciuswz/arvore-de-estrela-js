969f695717a2642dd655a04d6fdf60ea91c5a028
Vinícius Araújo <32021203+viniciuswz@users.noreply.github.com>
2019-05-14 11:53:59 -0300

    pequeno update

--- js.js
+++ js.js
@@ -13,3 +13,21 @@ while(i <= coluna){
     //console.log(arvore.push(linha))
     i++;
 }
+
+
+//se por acaso quiser dentro de u array é só usar essa parte:
+
+// while(i <= coluna){
+//     let arvore = [];
+//     let qtd = i -1;
+//     let metade =  coluna - i;
+//     let estrela = '*'.repeat(i);
+//     let outraEstrela = '*'.repeat(qtd);
+//     let outroEspaco = ''.repeat(qtd);
+//     let espaco =  ' '.repeat(metade);
+//     let tudo = espaco + estrela + outraEstrela + espaco;
+//     arvore.push(tudo)
+//     console.log(arvore)
+//     i++
+//      //console.log(i++)
+// }
